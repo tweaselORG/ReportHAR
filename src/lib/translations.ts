a41a55836d2d666ddb3cb582f037d49786975d72
Benjamin Altpeter <hi@bn.al>
2024-06-10 12:29:03 +0200

    Export translations

--- src/lib/translations.ts
+++ src/lib/translations.ts
@@ -1,12 +1,14 @@
 import { readFileSync } from 'fs';
 import type trackHarTranslationsEn from 'trackhar/i18n/en.json';
 
+/** The string translations. */
 export const translations = {
     en: JSON.parse(
         readFileSync(new URL('../../node_modules/trackhar/i18n/en.json', import.meta.url), 'utf-8')
     ) as typeof trackHarTranslationsEn,
 };
 
+/** The Typst template files. */
 export const templates = {
     en: {
         report: readFileSync(new URL('../../templates/en/report.typ', import.meta.url), 'utf-8'),
@@ -16,5 +18,10 @@ export const templates = {
     },
 };
 
-/** A language that translations are available for. */
+/** The languages that translations and templates are available for. */
+export const supportedLanguages = Object.keys(translations).filter((l) =>
+    Object.keys(templates).includes(l)
+) as SupportedLanguage[];
+
+/** A language that translations and templates are available for. */
 export type SupportedLanguage = keyof typeof templates & keyof typeof translations;
